457379f7f5202f5ca0d150a5b0ec1fae1498c89e
Hannu Kulmala <hannukul@student.uef.fi>
2025-06-12 14:05:07 +0300

    Add minimal register screen

--- app.rb
+++ app.rb
@@ -8,8 +8,18 @@ get '/login' do
   erb :login
 end
 
+get '/register' do
+  erb :register
+end
+
 post '/login' do
   username = params["username"]
   password = params["password"]
-  "you just send a request with #{username} and #{password}"
+  "you just send a login request with #{username} and #{password}"
+end
+
+post '/register' do
+  username = params["username"]
+  password = params["password"]
+  "you just send a register request with #{username} and #{password}"
 end
