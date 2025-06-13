4a6f1d0a685afd3dba45e0e24cc3321defc0b2d8
Hannu Kulmala <hannukul@student.uef.fi>
2025-06-13 13:33:14 +0300

    Add basic login functionality

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require 'sinatra'
+require 'sqlite3'
+
+db = SQLite3::Database.open "data.db"
+db.results_as_hash = true
+db.execute "CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)"
 
 get '/' do
   erb :index
@@ -15,11 +20,29 @@ end
 post '/login' do
   username = params["username"]
   password = params["password"]
-  "you just send a login request with #{username} and #{password}"
+  results = db.get_first_row "SELECT username, password FROM users WHERE username=? AND password=?", [username, password]
+
+  if results.nil?
+    puts "to console: user not found"
+    return "No user found"
+  else
+    puts results
+    session[:user_id] = results[0]
+
+    puts "to console: user found"
+    return "Welcome, #{username}!"
+  end
+
 end
 
 post '/register' do
   username = params["username"]
   password = params["password"]
-  "you just send a register request with #{username} and #{password}"
+
+  begin
+    db.execute("INSERT INTO users (username, password) VALUES (?, ?)", [username, password])
+    "Successfully added user #{username} to database"
+  rescue SQLite3::Exception => e
+    puts "Database error: #{e}"
+  end
 end
